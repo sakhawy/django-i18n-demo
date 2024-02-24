38db8778865170ba707f7005157316bcfafc67e6
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2024-02-25 01:14:29 +0200

    feat: add a hello_world translated template

--- demo/urls.py
+++ demo/urls.py
@@ -4,4 +4,5 @@ from demo import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path('hello/', views.hello_world_template, name='hello_world_template'),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.utils.translation import gettext as _
+
 
 def hello_world(request):
     return HttpResponse(_('Hello, World!'))
+
+def hello_world_template(request):
+    return render(request, 'demo/hello_world.html')
