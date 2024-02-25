bbc5f633969dac77b2c949e89e4f77f357859fa8
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2024-02-25 21:34:01 +0200

    feat: add a translated hello_world.js file

---
+++ demo/static/demo/js/hello_world.js
@@ -0,0 +1,1 @@
+alert(gettext("Hello, World!"));
--- demo/views.py
+++ demo/views.py
@@ -6,5 +6,6 @@ from django.utils.translation import gettext as _
 def hello_world(request):
     return HttpResponse(_('Hello, World!'))
 
+
 def hello_world_template(request):
     return render(request, 'demo/hello_world.html')
